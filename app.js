1400d3198cda43eb2b6a56f2e6db18677df2612a
koder <vineet15.yadav@lge.com>
2017-03-14 17:37:54 +0530

    example for express routing system

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 var express = require("express");
 var logger = require("morgan");
+var path = require("path");
 var http = require("http");
 
 var app = express();
 
+var publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
+
 app.use(logger("short"));
 
-app.use(function(request, response){
-		console.log("koder");
-		response.send("logged in");
+//routing using express
+app.get("/",function(request, response){
+	console.log("homepage");
+	response.send("Welcome to homepage");
 	
-}).listen(52280);
+});
+
+app.get("/aboutus",function(request, response){
+	console.log("about us page");
+	response.send("About us");
+	
+});
+
+app.use(function(request, response) {
+response.statusCode = 404;
+response.end("404!");
+});
+
+
+
+http.createServer(app).listen(52280);
