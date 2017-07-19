3b04b1c56296d40f3cd76bc5b7e2ccfae5d07d12
whykode <whykode@gmail.com>
2017-07-20 01:00:43 +0530

    Rest api versioning

---
+++ rest_api_versioning/api2.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var api = express.Router();
+
+api.get("/timezone", function(req, res) {
+res.send("Api v2: Sample response for /timezone");
+});
+
+
+api.get("/all_timezones", function(req, res) {
+res.send("Api v2:Sample response for /all_timezones");
+});
+
+module.exports = api;
---
+++ rest_api_versioning/app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var apiVersion1 = require('./api1.js')
+
+var apiVersion2 = require('./api2.js')
+
+var app = express();
+
+app.use("/v1", apiVersion1);
+
+app.use("/v2", apiVersion2);
+
+app.listen(40000,function(){
+	console.log('Server started on port 40000');
+});
